c3b6039ca50fe303a55a2c58c5ca8a4297a32ffc
Alex <hambiru@yandex.ru>
2022-08-09 15:07:24 +0300

    getStaticProps for burgers Page

--- pages/burgers/index.tsx
+++ pages/burgers/index.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { FC } from 'react'
 
-const Burgers = () => {
+import styles from '../../styles/Burgers.module.css'
+
+type BurgerType = {
+  name: string
+  image: string
+  desc: string
+  price: number
+  id: string
+}
+
+type BurgersPropsType = {
+  burgers: BurgerType[]
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch('http://localhost:5000/items')
+  const data = await res.json()
+  return {
+    props: {
+      burgers: data,
+    },
+  }
+}
+
+const Burgers: FC<BurgersPropsType> = ({ burgers }) => {
   return (
-    <div>Burgers</div>
+    <div>
+      <h1>Наши бургеры</h1>
+      {burgers.map((burger) => {
+        return (
+          <Link href={`/burgers/${burger.id}`} key={burger.id}>
+            <a className={styles.burgerCard}>
+              <div className={styles.imageContainer}>
+                <Image
+                  src={burger.image}
+                  alt={burger.name}
+                  width='100%'
+                  height='100%'
+                  layout='responsive'
+                  objectFit='cover'
+                />
+              </div>
+              <div>
+                <h3>{burger.name}</h3>
+                <p>{burger.desc}</p>
+              </div>
+            </a>
+          </Link>
+        )
+      })}
+    </div>
   )
 }
 
-export default Burgers;
+export default Burgers
