ab0d0de5f0727c5e3f664cd07e590e2c16c791e0
Alex Mercy <80210155+Alex-Mercy@users.noreply.github.com>
2022-08-24 19:42:52 +0300

    Update index.tsx

--- pages/burgers/index.tsx
+++ pages/burgers/index.tsx
@@ -17,7 +17,7 @@ type BurgersPropsType = {
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch('http://localhost:5000/items')
+  const res = await fetch('https://my-json-server.typicode.com/Alex-Mercy/burger-server/items')
   const data = await res.json()
   return {
     props: {
